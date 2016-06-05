3785e1e4e7633d9c2bb824b3bec5b1233cdc2c07
wickb <b.wick@techdivision.com>
2016-06-05 15:37:19 +0200

    added unit testing

---
+++ tests/Entities/TaskTest.php
@@ -0,0 +1,150 @@
+<?php
+
+/**
+ * \Wicked\Timely\Entities\TaskTest
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @author    Bernhard Wick <wick.b@hotmail.de>
+ * @copyright 2016 Bernhard Wick
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/wick-ed/timely
+ */
+
+namespace Wicked\Timely\Entities;
+
+/**
+ * Unit-Test class for the "Task" entity
+ *
+ * @author    Bernhard Wick <wick.b@hotmail.de>
+ * @copyright 2016 Bernhard Wick
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/wick-ed/timely
+ */
+class TaskTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Basic test for the constructor
+     *
+     * @return void
+     */
+    public function testConstructor()
+    {
+        $this->assertInstanceOf(
+            '\Wicked\Timely\Entities\Task',
+            new Task(
+                new Booking('test for a start booking'),
+                new Booking('test for an end booking'),
+                array()
+            )
+        );
+    }
+
+    /**
+     * Data provider for the testGetDurationWithNoSpecialCase method
+     *
+     * @return array
+     */
+    public function getDurationWithNoSpecialCaseProvider()
+    {
+        return array(
+            array(1465128900, 1465128998, 98),
+            array(1465125998, 1465128998, 3000),
+        );
+    }
+
+    /**
+     * Testing if the duration is calculated and returned correctly
+     *
+     * @param integer $startTime Start time in seconds
+     * @param integer $endTime   End time in seconds
+     * @param integer $duration  Duration in seconds
+     *
+     * @return void
+     *
+     * @dataProvider getDurationWithNoSpecialCaseProvider
+     */
+    public function testGetDurationWithNoSpecialCase($startTime, $endTime, $duration)
+    {
+        // get two bookings to make up the task
+        $startBooking = new Booking(
+            'test for a start booking',
+            'TEST-1',
+            date(Booking::DEFAULT_DATE_FORMAT, $startTime)
+        );
+        $endBooking = new Booking(
+            'test for a end booking',
+            'TEST-1',
+            date(Booking::DEFAULT_DATE_FORMAT, $endTime)
+        );
+
+        // make the assertion
+        $testTask = new Task($startBooking, $endBooking, array());
+        $this->assertEquals($duration, $testTask->getDuration());
+    }
+
+    /**
+     * Testing if the duration is calculated and returned correctly
+     *
+     * @return void
+     */
+    public function testGetDurationWithOneIntermediateTask()
+    {
+        // get two bookings to make up the task
+        $startBooking = new Booking(
+            'test for a start booking',
+            'TEST-1',
+            date(Booking::DEFAULT_DATE_FORMAT, 1465125998)
+        );
+        $endBooking = new Booking(
+            'test for an end booking',
+            'TEST-1',
+            date(Booking::DEFAULT_DATE_FORMAT, 1465128998)
+        );
+
+        // create two intermediate bookings to simulate a pause
+        $pauseStart = new Pause('pause start', false, 1465126998);
+        $pauseEnd = new Pause('pause end', true, 1465127998);
+
+        // make the assertion
+        $testTask = new Task($startBooking, $endBooking, array($pauseEnd, $pauseStart));
+        $this->assertEquals(2000, $testTask->getDuration());
+    }
+
+    /**
+     * Testing if the duration is calculated and returned correctly
+     *
+     * @return void
+     */
+    public function testGetDurationWithTwoIntermediateTasks()
+    {
+        // get two bookings to make up the task
+        $startBooking = new Booking(
+            'test for a start booking',
+            'TEST-1',
+            date(Booking::DEFAULT_DATE_FORMAT, 1465122998)
+        );
+        $endBooking = new Booking(
+            'test for an end booking',
+            'TEST-1',
+            date(Booking::DEFAULT_DATE_FORMAT, 1465128998)
+        );
+
+        // create two intermediate bookings to simulate a pause
+        $pauseStart1 = new Pause('pause start', false, 1465123998);
+        $pauseEnd1 = new Pause('pause end', true, 1465124498);
+        $pauseStart2 = new Pause('pause start', false, 1465125998);
+        $pauseEnd2 = new Pause('pause end', true, 1465126998);
+
+        // make the assertion
+        $testTask = new Task($startBooking, $endBooking, array($pauseEnd1, $pauseStart1, $pauseEnd2, $pauseStart2));
+        $this->assertEquals(4500, $testTask->getDuration());
+    }
+}
